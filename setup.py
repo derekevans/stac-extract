9e455ec856e0ba4c11358d92797fa1d4adb74658
Derek Evans <derekevans@Dereks-MacBook-Air.local>
2025-05-19 06:32:21 -0500

    updated dependencies

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description_content_type='text/markdown',
     install_requires=[
         'numpy>=1.24.0',
-        'geopandas==0.14.0',
+        'geopandas==1.0.1',
         'pyyaml',
         'pystac-client',
 
