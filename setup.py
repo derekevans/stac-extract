9f80e349a45bea92ab355dc7ab5f41f89c71ebea
homelab <derekae88@gmail.com>
2025-05-22 08:19:04 -0500

    updated dependencies

--- setup.py
+++ setup.py
@@ -13,14 +13,14 @@ setup(
     long_description_content_type='text/markdown',
     install_requires=[
         'numpy>=1.24.0',
-        'geopandas==1.0.1',
+        'geopandas>=1.0.1',
         'pyyaml',
         'pystac-client',
         'GDAL==3.11.0',
         'alive_progress',
-        'netCDF4==1.6.5',
-        'xarray==2024.1.1',
-        'rioxarray==0.15.1'
+        'netCDF4>=1.6.5',
+        'xarray>=2024.1.1',
+        'rioxarray>=0.15.1'
     ],
     python_requires='>=3.10.0',
     scripts=[
